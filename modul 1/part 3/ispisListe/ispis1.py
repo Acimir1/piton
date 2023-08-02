9674deba5cf175d69beed338b362754d011676d0
Acimir1 <111501285+Acimir1@users.noreply.github.com>
2023-08-02 11:36:29 +0200

    test

---
+++ modul 1/part 3/ispisListe/ispis1.py
@@ -0,0 +1,4 @@
+Var_list = [34,56,78,89]
+
+for j,k in enumerate(Var_list):
+    print(j,k)
---
+++ modul 1/part 3/merging dictionaries/spajanjedict3.py
@@ -0,0 +1,8 @@
+Vardict_1 = {1: 'a', 2: 'l'}
+Vardict_2 = {2: 'm', 3: 'p'}
+
+Vardict_3 = Vardict_2.copy()
+
+Vardict_3.update(Vardict_1)
+
+print(Vardict_3)
