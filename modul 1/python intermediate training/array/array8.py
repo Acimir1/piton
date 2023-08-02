4940cedf99b867ee8a2bb9e6e19efbbc91bf40b8
Acimir1 <111501285+Acimir1@users.noreply.github.com>
2023-08-02 19:33:49 +0200

    Update array8.py

--- modul 1/python intermediate training/array/array8.py
+++ modul 1/python intermediate training/array/array8.py
@@ -1,4 +1,4 @@
-Varx = [[1,2,3],[4,5,6],[7,8,9]]
+Varx = [[1,2,3],[4,5,6],[6,8,9]]
 Vary = [[2,3,4],[5,6,7],[8,9,1]]
 Varresult = [[Varx[j][k] + Vary[j][k] for j in range(len(Varx[0]))] for k in range(len(Varx))]
 
