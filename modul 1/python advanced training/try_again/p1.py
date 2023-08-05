8c379b123ad9de1a0a09c0d8eddce4fede8c7f7c
Acimir1 <111501285+Acimir1@users.noreply.github.com>
2023-08-05 10:23:21 +0200

    Update p1.py

--- modul 1/python advanced training/try_again/p1.py
+++ modul 1/python advanced training/try_again/p1.py
@@ -1,7 +1,16 @@
-Vars = (str(input("Enter name of text file: ")))
-Varfile = open(Vars, 'r')
-Varline = Varfile.readline()
-while(Varline != ""):
-    print(Varline)
-    Varline = Varfile.readline()
-Varfile.close
+import os
+
+file_name = "1.txt"
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+file_path = os.path.join(current_directory, file_name)
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"File '{file_name}' not found.")
+except Exception as e:
+    print(f"Error occurred: {e}")
